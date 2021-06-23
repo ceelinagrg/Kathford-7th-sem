68f0ec7b85ac851183e837c560a69bba334c76ab
ceelinagrg <gsalina950@gmail.com>
2021-06-23 19:30:36 +0545

    Javascript lecture

---
+++ function/fun.js
@@ -0,0 +1,4 @@
+function area(){
+    return 'Hello'
+}
+console.log(area())
---
+++ kath/variable.js
@@ -0,0 +1,9 @@
+// var num1 = 100
+// let num2 = 200
+// const num3 = 300
+// console.log(num1, num2, num3)
+let num1 = 13
+const num2 = 20
+num1 = 40
+console.log(num1)
+console.log(num2)
